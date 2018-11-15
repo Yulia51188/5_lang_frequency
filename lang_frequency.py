6ae483802b16a320d20227c4086ca6af202f4f94
Yulia51188 <sun51188@gmail.com>
2018-11-15 09:22:13 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,13 @@
 from collections import Counter
 import argparse
 import string
+import os
+
+
+def load_data_from_file(filepath):
+    with open(filepath, 'r') as file_object:
+        str_data = file_object.read()
+    return str_data
 
 
 def get_most_frequent_words(text, output_word_count):
@@ -11,6 +18,7 @@ def get_most_frequent_words(text, output_word_count):
     word_statistic = Counter(word_list).most_common(output_word_count)
     return word_statistic
 
+
 def check_is_natural(input_string, default_value=10):
     if not input_string.isdigit():
         return default_value
@@ -19,14 +27,21 @@ def check_is_natural(input_string, default_value=10):
     else:
         return default_value
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='The word frequency counter.')
+    parser = argparse.ArgumentParser(
+        description='The word frequency counter.'
+    )
     parser.add_argument(
-        'text_file', type=argparse.FileType(),
+        'filepath',
+        type=str,
         help='a path to the text file where the script counts word frequency'
     )
     parser.add_argument(
-        '-c', '--output_word_count', type=check_is_natural, default=10,
+        '-c',
+        '--output_word_count',
+        type=check_is_natural,
+        default=10,
         help='how many words with statistic print as a result'
     )
     return parser.parse_args()
@@ -34,7 +49,9 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
-    text = args.text_file.read()
+    if not os.path.exists(args.filepath):
+        exit("File doesn't exist")
+    text = load_data_from_file(args.filepath)
     if text is '':
         exit("The file is empty")
     for word, frequency in get_most_frequent_words(text, args.output_word_count):
